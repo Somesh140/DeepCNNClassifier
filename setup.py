1e07c10064d4c96dcc0ed90dfa9491f460bc48dc
Somesh140 <somesh140@gmail.com>
2022-09-23 15:43:07 +0530

    setup complete

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+
+with open("README.md","r",encoding = "utf-8") as f:
+    long_description = f.read()
+
+__version__="0.0.0"
+
+REPO_NAME = "DeepCNNClassifier"
+AUTHOR_USER_NAME ="Somesh140"
+SRC_REPO = "deepClassifier"
+AUTHOR_EMAIL= "Somesh140@gmail.com"
+
+setup(
+    name=SRC_REPO,
+    version= __version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src")
+)
